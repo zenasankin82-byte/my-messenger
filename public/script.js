7067c68cc1c6d542f5272925c8e734b9dfeea558
Evgeniy Vladimirovich <zenasankin82@gmail.com>
2026-02-28 13:59:15 +0700

    fix html

--- public/script.js
+++ public/script.js
@@ -4,25 +4,41 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const messages = document.getElementById("messages");
 
+// спрашиваем имя один раз
+let username = localStorage.getItem("username");
+
+if (!username) {
+  username = prompt("Введите ваше имя:");
+  localStorage.setItem("username", username);
+}
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+
   if (input.value) {
-    socket.emit("message", input.value);
+    socket.emit("message", {
+      username: username,
+      text: input.value
+    });
+
     input.value = "";
   }
 });
 
+// новое сообщение
 socket.on("message", function (msg) {
   const item = document.createElement("li");
-  item.textContent = msg;
+  item.textContent = msg.username + ": " + msg.text;
   messages.appendChild(item);
 });
 
+// загрузка старых сообщений
 socket.on("load messages", function (msgs) {
   messages.innerHTML = "";
+
   msgs.forEach((msg) => {
     const item = document.createElement("li");
-    item.textContent = msg.text;
+    item.textContent = msg.username + ": " + msg.text;
     messages.appendChild(item);
   });
 });
